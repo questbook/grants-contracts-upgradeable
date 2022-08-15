c630711e3a5b50e1d2a5a5849734461fe7cbf0e7
swetshaw <swt.shw94@gmail.com>
2022-08-15 15:44:24 +0530

    chore: add doc comments for batchUpdateApplicationState function

--- contracts/ApplicationRegistry.sol
+++ contracts/ApplicationRegistry.sol
@@ -222,6 +222,12 @@ contract ApplicationRegistry is Initializable, UUPSUpgradeable, OwnableUpgradeab
         );
     }
 
+    /**
+     * @notice Batch update application state
+     * @param _applicationsIds an array of target applicationIds for which state needs to be updated
+     * @param _applicationStates an array of updated states for the applications
+     * @param _workspaceId workspace id of application's grant
+     */
     function batchUpdateApplicationState(
         uint96[] memory _applicationIds,
         ApplicationState[] memory _applicationStates,
